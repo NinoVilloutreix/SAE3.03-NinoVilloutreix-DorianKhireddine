5e9909cfe4e9b8fa062fc2899e22d91761adb7d2
NinoVilloutreix <nino.villoutreix@etu.unilim.fr>
2025-12-14 15:09:20 +0100

    added first animation

--- src/main.js
+++ src/main.js
@@ -1,5 +1,28 @@
 import gsap from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger"; // Pour le plugin ScrollTrigger
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 import "./style.css";
 
+gsap.registerPlugin(ScrollTrigger);
+
 gsap.to(".object", { rotation : 360, duration: 2, repeat: -1, ease: "inoutcirc", yoyo: true})
+
+
+
+const basictl = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".img__planet",   
+    start: "top 80%",          
+    end: "bottom 30%",     
+    markers: true,            
+    toggleActions: "play reverse play reverse"
+    // play = lance l’anim en descendant
+    // reverse = rejoue en sens inverse quand tu remontes
+  },
+  yoyo: true
+});
+
+basictl.fromTo(
+  ".img__planet",
+  { x: 500, opacity: 0, scale: 1 },
+  { x: 0, opacity: 1, duration: 1, scale: 3 }
+);
